aa27536b82a94ed5cd9cb2f4ea27423f3b2bb854
Vikrant Singh Guleria <vikrantsinghguleria1999@gmail.com>
2025-03-08 17:28:13 +0530

    Panel Updated

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// const changeBgRed = () => {
-//     document.body.firstElementChild.style.background = "red"
-// }
-
-
-// let b = document.body
-// console.log("First child of b is", b.firstChild)
-// console.log("First element child of b is", b.firstElementChild)
+document.querySelector('.fashion-dropdown').addEventListener('click', function (e) {
+    e.preventDefault();
+    // This prevents the default action of the click event. If your dropdown trigger 
+    // is a link (<a> tag), 
+    // this stops it from navigating to a new page.
+    this.classList.toggle('active');
+    // This toggles the 'active' class on the clicked element. If the element doesn't 
+    // have the 'active' class, it adds it; if it already has it, it removes it.
+});
